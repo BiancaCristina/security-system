565abedebd5c51b5550d9ad20e9518551bc0b68a
Bianca <biancacristina-silva@outlook.com>
2020-02-20 13:40:09 -0300

    Included Vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
@@ -17,6 +18,7 @@ Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
     vuetify,
     render: h => h(App)
 }).$mount('#app')
