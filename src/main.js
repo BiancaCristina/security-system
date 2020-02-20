c2c05965f3485b3c64a92735079bdf12f013905e
Bianca <biancacristina-silva@outlook.com>
2020-02-20 12:11:39 -0300

    Included Font Awesome 5

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { dom } from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas)
+library.add(fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+dom.watch()
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    router,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
